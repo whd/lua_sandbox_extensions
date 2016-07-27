eac6f36e096ad49391342b607bfb17380bdaeac6
Wesley Dawson <whd@mozilla.com>
2016-07-27 15:38:29 -0700

    Add H1 to payload size filter and use read_message("size")

--- moz_telemetry/sandboxes/heka/analysis/moz_telemetry_payload_size.lua
+++ moz_telemetry/sandboxes/heka/analysis/moz_telemetry_payload_size.lua
@@ -3,6 +3,8 @@
 -- file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 --[[
+# Mozilla Telemetry Payload Size
+
 Extract submission sizes and counts for pipeline messages, emitting small
 derived messages for reporting.
 
@@ -31,7 +33,7 @@ function process_message()
     msg.Fields.build = read_message("Fields[appBuildId]")
     msg.Fields.channel = read_message("Fields[appUpdateChannel]")
     msg.Fields.docType = read_message("Fields[docType]")
-    msg.Fields.size = read_message("Fields[Size]")
+    msg.Fields.size = read_message("size")
 
     -- This could be computed from msg.Timestamp, but we need the field for
     -- partitioning the data in the S3 Output.
